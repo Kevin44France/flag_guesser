2b89a8a8e755d364dd64e8b9435736f33013f34a
Lygerity <louison.tendron.gaia@gmail.com>
2024-01-15 14:48:58 +0100

    2ème jeu fini

--- lib/ui/screens/page2.dart
+++ lib/ui/screens/page2.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 import '../../models/country.dart';
 
@@ -34,7 +35,8 @@ class _Page2State extends State<Page2> {
         setState(() {
           countries = data
               .map((json) => Country(
-                  name: json['name']['common'], flagUrl: json['flag']['officialflag']['svg']))
+                  name: json['name']['shortnamelowercase'],
+                  flagUrl: json['flag']['officialflag']['svg']))
               .toList();
           isLoading = false;
         });
@@ -50,20 +52,62 @@ class _Page2State extends State<Page2> {
         isLoading = false;
       });
     }
+    setChoices();
   }
 
-  void setChoices(){
-    choices.clear();
-    answer = countries[Random().nextInt(countries.length)];
-    choices = generateRandomChoices(countries, answer);
+  void setChoices() {
+    setState(() {
+      choices.clear();
+      answer = countries[Random().nextInt(countries.length)];
+      choices = generateRandomChoices(countries, answer);
+    });
   }
 
-  List<Country> generateRandomChoices(List<Country> countries, Country answer){
+  Future<void> checkValues(Country choice) async {
+    if (choice == answer) {
+      await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Bravo!'),
+              content: const Text('Tu as trouvé le bon drapeau!'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
+    } else {
+      await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Oops!'),
+              content: const Text('Ce n\'est pas le bon drapeau!'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
+    }
+    setChoices();
+  }
+
+  List<Country> generateRandomChoices(List<Country> countries, Country answer) {
     List<Country> choices = [];
     choices.add(answer);
-    while(choices.length < 4){
+    while (choices.length < 4) {
       Country choice = countries[Random().nextInt(countries.length)];
-      if(!choices.contains(choice)){
+      if (!choices.contains(choice)) {
         choices.add(choice);
       }
     }
@@ -75,34 +119,70 @@ class _Page2State extends State<Page2> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Page 2'),
+        title: const Text('Devine le drapeau'),
         backgroundColor: Colors.green,
       ),
       body: Center(
-       child: isLoading ? const CircularProgressIndicator() : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            const Text('Devine le drapeau correspondant au pays!',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              child: const Text('Commencer le jeu'),
-              onPressed: () {
-                fetchCountries();
-              },
-            ),
-            Expanded(
-              child:
-            ),
-            ElevatedButton(
-              child: const Text('Retour au Menu Principal'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : choices.isNotEmpty
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const Text(
+                        'Devine le drapeau correspondant au pays!',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        answer.name,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 8.0,
+                          mainAxisSpacing: 8.0,
+                        ),
+                        itemCount: choices.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            onTap: () {
+                              checkValues(choices[index]);
+                            },
+                            child: Column(
+                              children: [
+                                SvgPicture.network(
+                                  choices[index].flagUrl,
+                                  width: 100.0,
+                                  height: 60.0,
+                                  fit: BoxFit.contain,
+                                ),
+                                // Text(
+                                //   choices[index].name,
+                                //   style: const TextStyle(
+                                //       fontSize: 16,
+                                //       fontWeight: FontWeight.bold),
+                                // ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  )
+                : const Text('Appuyer sur le bouton pour commencer le jeu'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchCountries,
+        tooltip: 'Fetch Random Country',
+        child: const Icon(Icons.refresh),
       ),
     );
   }
